50522a1fd0dc177da04288242f1195c9068dfd22
Hayden Feddock <hayden4feddock@gmail.com>
2023-11-15 22:02:44 -0500

    removed unnecessary classes and optimized code

--- main.ino
+++ main.ino
@@ -15,83 +15,61 @@
 Adafruit_HX8357 tft = Adafruit_HX8357(TFT_CS, TFT_DC, TFT_RST);
 
 /******************** CLASSES ********************/
-class Chord {
-  private:
-    String chordName;
-
-  public:
-    Chord() {};
-    Chord(String name) : chordName(name) {}
-    String getChordName() { return chordName; }
-    String getFingerPositions() {
-      if (chordName == "G Major") return "320003";
-      if (chordName == "C Major") return "x32010";
-      return "Unknown";
-    }
-};
-
-class Beat {
-  private:
-    Chord chord;
-    String lyric;
-
-  public:
-    Beat() {};
-    Beat(String chordName, String text) : chord(Chord(chordName)), lyric(text) {}
-    Chord getChord() { return chord; }
-    String getLyric() { return lyric; }
-};
-
 class Song {
   private:
     String filename;
     unsigned long filePosition;
-    Beat* beatBuffer;
+    String* chordBuffer;
+    String* lyricBuffer;
     int beatCount;
 
   public:
-    Song(String file) : filename(file), filePosition(0), beatBuffer(nullptr), beatCount(0) {}
+    Song(String file) : filename(file), filePosition(0), chordBuffer(nullptr), lyricBuffer(nullptr), beatCount(0) {}
 
     ~Song() {
-      if (beatBuffer != nullptr) {
-        delete [] beatBuffer;
+      if (chordBuffer != nullptr) {
+        delete [] chordBuffer;
+      }
+      if (lyricBuffer != nullptr) {
+        delete [] lyricBuffer;
       }
     }
 
     bool loadNextNBeats(int n) {
 
       if (!SD.begin(TFT_CS)) {
-        Serial.println("Initialization failed!");
+        Serial.println(F("Initialization failed!"));
         return;
       }
-      Serial.println("Initialization done.");
+      Serial.println(F("Initialization done."));
 
       File file = SD.open(filename);
 
       if (!file) {
-        Serial.println("File Error");
+        Serial.println(F("File Error"));
         return false;
       }
 
       file.seek(filePosition);
 
-      if (beatBuffer != nullptr) {
-        delete [] beatBuffer;
+      if (chordBuffer != nullptr) {
+        delete [] chordBuffer;
+      }
+      if (lyricBuffer != nullptr) {
+        delete [] lyricBuffer;
       }
 
-      beatBuffer = new Beat[n];
+      chordBuffer = new String[n];
+      lyricBuffer = new String[n];
       beatCount = n;
 
-      String chordName;
-      String lyric;
       for (int i=0; i<n; i++) {
         if (file.available()) {
-          chordName = file.readStringUntil(',');
+          chordBuffer[i] = file.readStringUntil(',');
           // Serial.println(chordName);
-          lyric = file.readStringUntil('\n');
+          lyricBuffer[i] = file.readStringUntil('\n');
           // Serial.println(lyric);
-          beatBuffer[i] = Beat(chordName, lyric);
-        } else {                                  // End of file
+        } else {
           beatCount = i;
           break;
         }
@@ -103,14 +81,12 @@ class Song {
 
     int getBeatCount() { return beatCount; }
 
-    Beat* getBeats() {
-      return beatBuffer;
+    String* getChordBuffer() {
+      return chordBuffer;
     }
 
-    Beat getBeat(int n) {
-      if (n < beatCount) {
-        return beatBuffer[n];
-      }
+    String* getLyricBuffer() {
+      return lyricBuffer;
     }
 };
 
@@ -124,49 +100,19 @@ void setup() {
   tft.begin();
   tft.setRotation(1);
   tft.fillScreen(HX8357_BLACK);
-  tft.setCursor(0, 0);
-  tft.setTextColor(HX8357_WHITE); tft.setTextSize(2);
-  tft.print("Hello World!");
-  Serial.println(tft.getCursorX());
-  Serial.println(tft.getCursorY());
 
   String songFilename = "test.csv";
   Song song(songFilename);
 
   song.loadNextNBeats(8);
-
-  // for(uint8_t rotation=0; rotation<4; rotation++) {
-  //   Serial.print("rotation = ");
-  //   Serial.println(rotation);
-  //   tft.setRotation(rotation);
-  //   testText();
-  //   int w = tft.width();
-  //   Serial.print("w =");
-  //   Serial.println(w);
-  //   int h = tft.height();
-  //   Serial.print("h = ");
-  //   Serial.println(h);
-  //   delay(1000);
-  // }
-
-  
-  String text = song.getBeat(0).getChord().getChordName();
-  Serial.println(text);
-  tft.println(text);
-
-  printBeats(song.getBeats(), song.getBeatCount());
-
-  
-
-  
-
+  printBeats(song.getChordBuffer(), song.getLyricBuffer(), song.getBeatCount());
 }
 
 void loop() {
   
 }
 
-bool printBeats(Beat* beats, int beatCount) {
+bool printBeats(String* chordBuffer, String* lyricBuffer, int beatCount) {
   tft.fillScreen(HX8357_BLACK);
   int interval = tft.width()/beatCount;
 
@@ -175,7 +121,7 @@ bool printBeats(Beat* beats, int beatCount) {
     tft.setCursor(i*interval, 0);
     tft.setTextColor(HX8357_WHITE);
     tft.setTextSize(2); // Character Size 2: 10 x 14
-    tft.print(beats[i].getChord().getChordName());
+    tft.print(chordBuffer[i]);
   }
 
   // Print of the line of lyrics
@@ -183,6 +129,6 @@ bool printBeats(Beat* beats, int beatCount) {
     tft.setCursor(i*interval, 16);
     tft.setTextColor(HX8357_WHITE);
     tft.setTextSize(2); // Character Size 2: 10 x 14
-    tft.print(beats[i].getLyric());
+    tft.print(lyricBuffer[i]);
   }
 }
